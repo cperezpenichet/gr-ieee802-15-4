fe05aa97162ffd9a8e53da782537fc61e6a14330
Carlos Penichet <cpp@fe.up.pt>
2014-04-15 11:13:51 +0100

    Added module to compute BER by comparing two PCAPs

---
+++ contrib/ber.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+import glob
+
+import pure_pcapy
+
+#PCAP_FILE = '/tmp/sensor_chips.pcap'
+NOISY_PCAP = '/tmp/sensor_chips_06.pcap'
+BASE_PCAP  = '/tmp/sensor_chips_01.pcap'
+
+TIME_EPS   = 1e-2
+
+def get_time(header, start_time=0):
+	t = header.getts()
+	return t[0] + t[1]*1e-6 - start_time
+
+def one_count(n):
+	cnt = 0
+	while n > 0:
+		cnt += 1
+		n &= (n-1)
+	return cnt
+
+def compute_BER(pcap_file):
+	n_pcap = pure_pcapy.open_offline(pcap_file)
+	b_pcap = pure_pcapy.open_offline(BASE_PCAP)
+
+	error_bits, total_bits = 0, 0
+	n_header, n_payload = n_pcap.next()
+	b_header, b_payload = b_pcap.next()
+	if n_header == None:
+		return float("inf"), 0, float("inf")
+	n_start, b_start = get_time(n_header), get_time(b_header)
+	while n_header != None and b_header != None:
+		n_time = get_time(n_header, n_start)
+		b_time = get_time(b_header, b_start)
+
+		while b_header != None and n_time > b_time-TIME_EPS and (abs(n_time - b_time) > TIME_EPS):
+			b_header, b_payload = b_pcap.next()
+			b_time = get_time(b_header, b_start)
+		if (abs(n_time - b_time) > TIME_EPS):
+			n_header, n_payload = n_pcap.next()
+			continue
+		
+		n_payload = bytearray(n_payload)
+		b_payload = bytearray(b_payload)
+
+		total_bits += 8 * len(n_payload)
+		for i in range(len(n_payload)):
+			error_bits += one_count(n_payload[i]^b_payload[i])
+
+		#print b_time, n_time, error_bits
+		n_header, n_payload = n_pcap.next()
+
+	return error_bits, total_bits, float(error_bits)/total_bits
+
+if __name__ == '__main__':
+	for pcap in sorted(glob.glob('/tmp/*.pcap')):
+		print "%s,%s" % (pcap, ','.join(map(str, compute_BER(pcap))))
