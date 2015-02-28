387b3854a9258fab93d10c7e62c26a0c23c5e0cd
Carlos Pérez Penichet <cperezpenichet@gmail.com>
2015-02-28 10:44:54 +0000

    BER script now computes based on predefined bytes
    Updated the BER-cumputing script to compute based
    on a predefined byte sequence instead of trying
    to align two pcap packet sequences.
    
    Conflicts:
    	contrib/ber.py

--- contrib/ber.py
+++ contrib/ber.py
@@ -6,9 +6,26 @@ from os import sep
 import pure_pcapy
 
 PCAP_DIR   = '/tmp'
-BASE_PCAP  = PCAP_DIR + '/sensor_chips_01.pcap'
 
-TIME_EPS   = 1e-2
+CHIP_MAPPING = [
+        1618456172,
+        1309113062,
+        1826650030,
+        1724778362,
+        778887287,
+        2061946375,
+        2007919840,
+        125494990,
+        529027475,
+        838370585,
+        320833617,
+        422705285,
+        1368596360,
+        85537272,
+        139563807,
+        2021988657]
+
+SYMBOLS = (0xA0, 0x09, 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF)
 
 def get_time(header, start_time=0):
 	t = header.getts()
@@ -26,43 +43,43 @@ def get_num(x):
 
 def compute_BER(pcap_file):
 	n_pcap = pure_pcapy.open_offline(pcap_file)
-	b_pcap = pure_pcapy.open_offline(BASE_PCAP)
 
 	error_bits, total_bits = 0, 0
 	n_header, n_payload = n_pcap.next()
-	b_header, b_payload = b_pcap.next()
 	if n_header == None:
 		return float("inf"), 0, float("inf")
-	n_start, b_start = get_time(n_header), get_time(b_header)
+	n_start = get_time(n_header)
 	while n_header != None:
-		n_time = get_time(n_header, n_start)
-		
-		while True:
-			b_time = get_time(b_header, b_start)
-			if b_header == None or \
-			   b_time-n_time > TIME_EPS or \
-			   (abs(n_time-b_time) <= TIME_EPS and \
-				b_header.getlen() == n_header.getlen()\
-			   ):
-				break
-			b_header, b_payload = b_pcap.next()
-		if b_header == None or (abs(n_time - b_time) > TIME_EPS):
+		if n_header.getlen()-3*8 != len(SYMBOLS)*8:
 			n_header, n_payload = n_pcap.next()
 			continue
 		
+		n_time = get_time(n_header, n_start)
 		n_payload = bytearray(n_payload)
-		b_payload = bytearray(b_payload)
 
-		total_bits += 8 * len(n_payload)
-		for i in range(len(n_payload)):
-			error_bits += one_count(n_payload[i]^b_payload[i])
+		total_bits += 8 * (len(n_payload)-3*8)
 
-		#print b_time, n_time, error_bits
+		for i in range(len(SYMBOLS)):
+			H, L = CHIP_MAPPING[SYMBOLS[i]>>4], CHIP_MAPPING[SYMBOLS[i]&0x0F]
+			tt = ''
+			for j in range(8*(i+1)-1,8*i-1,  -1):
+				tt += "{:02X}".format(n_payload[j])
+				if j%4==0:
+					tt += " "
+			h, l = map(lambda x: int(x, 16),tt.split())
+		
+			error_bits += one_count((H^h)&0x7FFFFFFE)
+			error_bits += one_count((L^l)&0x7FFFFFFE)
+			total_bits -= 4
+		#	print n_time, error_bits, total_bits, '{:032b}'.format((H^h)&0x7FFFFFFE), '{:032b}'.format((L^l)&0x7FFFFFFE)
+		#print
+
 		n_header, n_payload = n_pcap.next()
 
-	return error_bits, total_bits, float(error_bits)/total_bits
+	return error_bits, total_bits, float(error_bits)/total_bits if total_bits > 0 else float('inf')
 
 if __name__ == '__main__':
 	for pcap in sorted(glob(sep.join((PCAP_DIR, '*_chips_[0-9][0-9].pcap')))):
+		#pcap = "/tmp/sensor_chips_36.pcap"
 		print "%s,%s" % (get_num(pcap), ','.join(map(str, compute_BER(pcap))))
 
